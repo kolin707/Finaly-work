09407a6bac9ee86708b8939e861e63dd2ba68918
kolin707 <kolin_006@mail.ru>
2021-12-09 20:15:50 +0300

    fragments3

--- app/src/main/java/com/kolin/applicationfragment/MainActivity.kt
+++ app/src/main/java/com/kolin/applicationfragment/MainActivity.kt
@@ -4,68 +4,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private const val TAG = "MyTag"
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var switchFragmentsButton : Button
+    lateinit var bottomNavigationMenu: BottomNavigationView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        switchFragmentsButton = findViewById(R.id.switch_fragment_button)
 
-        Log.d(TAG,"MainActivity - onCreate")
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation_menu)
 
-        val startFragment = StartFragment()
-        val endFragment = EndFragment()
-        switchFragmentsButton.setOnClickListener {
-            val fragment =
-                when (supportFragmentManager.findFragmentById(R.id.fragment_container)){
-                    is StartFragment-> endFragment
-                    is EndFragment->startFragment
-                    else ->startFragment
+        bottomNavigationMenu.setOnItemSelectedListener { item ->
+            var fragment: Fragment? =null
+            when (item.itemId) {
+                R.id.fragment_1 -> {
+                    fragment = FirstFragment()
                 }
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container,fragment)
-                .addToBackStack(fragment.tag)
-                .commit()
-        }
+                R.id.fragment_2 -> {
+                    fragment = SecondFragment()
+                }
 
-    }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "MainActivity - onStart")
-    }
+                R.id.fragment_3 -> {
+                    fragment = ThirdFragment()
+                }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "MainActivity - onResume")
-    }
+            }
+            replaceFragment(fragment!!)
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG,"MainActivity - onPause")
-    }
+            true
 
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG,"MainActivity - onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG,"MainActivity - onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG,"MainActivity - onDestroy")
+            }
 
 
     }
+    fun replaceFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(fragment.tag)
+            .commit()
+
+    }
+
 
 }
+
--- app/src/main/java/com/kolin/applicationfragment/ThirdFragment.kt
+++ app/src/main/java/com/kolin/applicationfragment/ThirdFragment.kt
@@ -1,22 +1,21 @@
 package com.kolin.applicationfragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class EndFragment : Fragment() {
+
+class ThirdFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_end, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_third, container, false)
 
         return view
     }
-
-
 }
