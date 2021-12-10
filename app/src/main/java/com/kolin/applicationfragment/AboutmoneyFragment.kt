502f42993c30ae1bb007ddb66367911f30afb900
kolin707 <kolin_006@mail.ru>
2021-12-10 15:31:36 +0300

    first commit

--- app/src/main/java/com/kolin/applicationfragment/AboutmoneyFragment.kt
+++ app/src/main/java/com/kolin/applicationfragment/AboutmoneyFragment.kt
@@ -1,9 +1,12 @@
 package com.kolin.applicationfragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class AboutmoneyFragment : Fragment() {
     lateinit var aboutmoneyRecyclerView: RecyclerView
+    lateinit var showWebSiteButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,11 +34,17 @@ class AboutmoneyFragment : Fragment() {
             "8. South African Rand (ZAR)"
         )
 
+        showWebSiteButton=view.findViewById(R.id.go_odin)
         aboutmoneyRecyclerView = view.findViewById(R.id.about_money_recycler)
         aboutmoneyRecyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         aboutmoneyRecyclerView.adapter = MoneyAdapter(MoneyNames)
 
+        showWebSiteButton.setOnClickListener {
+            val link = Uri.parse("https://odin.study/connect")
+            val intent = Intent(Intent.ACTION_VIEW, link)
+            context?.startActivity(intent)
+        }
 
         return view
     }
